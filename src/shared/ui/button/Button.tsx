1e911d04280a8cbcf51ceccf4bb90d4d4a36576a
Matthew Kalinin <matvey.kalinin2001@gmail.com>
2022-08-12 12:29:16 +0300

    FIX add button type

--- src/shared/ui/button/Button.tsx
+++ src/shared/ui/button/Button.tsx
@@ -5,17 +5,18 @@ interface ButtonProps {
     text: string;
     size?: 'normal' | 'big';
     disabled?: boolean;
+    type?: 'submit' | 'reset' | 'button';
     onButtonClick?: () => void;
 }
 
-export const Button = ({text, onButtonClick, size = 'normal', disabled = false}: ButtonProps) => {
+export const Button = ({text, onButtonClick, size = 'normal', disabled = false, type}: ButtonProps) => {
     const buttonClass = classNames(styles.button, {[styles.big]: size === 'big'}, {[styles.normal]: size === 'normal'});
     const handleClick = () => {
         if (onButtonClick) onButtonClick();
     };
 
     return (
-        <button onClick={handleClick} className={buttonClass} disabled={disabled}>
+        <button onClick={handleClick} className={buttonClass} disabled={disabled} type={type}>
             {text}
         </button>
     );
